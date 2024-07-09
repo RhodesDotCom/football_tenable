a30b3982ec2ae8670af9f7875c00453f5835d4f1
nathan <nathan.rhodes56@gmail.com>
2024-07-09 18:26:06 +0100

    removing conflict messages

--- stats-db/init.sql
+++ stats-db/init.sql
@@ -29,46 +29,12 @@ CREATE TABLE IF NOT EXISTS player_stats (
     penalties_missed FLOAT,
     position VARCHAR(255)
 );
-<<<<<<< HEAD
-DO $$ 
-BEGIN
-    IF NOT EXISTS (SELECT 1 FROM player_stats LIMIT 1) THEN
-        COPY player_stats FROM '/docker-entrypoint-initdb.d/csv_data/players_formatted_int.csv' DELIMITER ',' CSV HEADER;
-    END IF;
-END $$;
-=======
->>>>>>> feature/database_normalisation
-
-
-DO $$
-BEGIN
-    DELETE FROM player_stats WHERE team = '2 Teams';
-    COPY player_stats FROM '/docker-entrypoint-initdb.d/csv_data/two_teams_fixes.csv' DELIMITER ',' CSV HEADER;
-END $$;
 
 CREATE TABLE IF NOT EXISTS countries (
     country_code VARCHAR(3) NOT NULL,
     country VARCHAR(255) NOT NULL,
     primary key(country_code, country)
 );
-
-<<<<<<< HEAD
-DO $$
-BEGIN
-    IF NOT EXISTS (SELECT 1 FROM countries LIMIT 1) THEN
-        COPY countries FROM '/docker-entrypoint-initdb.d/csv_data/countries.csv' DELIMITER ',' CSV HEADER;
-    END IF;
-END $$;
-
-CREATE OR REPLACE VIEW player_goals_by_season_ranked as
-	select 
-		player,
-		season,
-		goals,
-		ROW_NUMBER() OVER (PARTITION BY season ORDER BY goals DESC) AS rn
-	from player_stats ps
-	where goals is not null;
-=======
 
 CREATE OR REPLACE VIEW player_goals_by_season_ranked as
     select 
@@ -81,7 +47,6 @@ CREATE OR REPLACE VIEW player_goals_by_season_ranked as
     join players p 
     on p.player_id = ps.player_id
     where goals is not null;
->>>>>>> feature/database_normalisation
 
 CREATE OR REPLACE VIEW goals_by_country_ranked as 
     select country, sum(goals) as total_goals
@@ -89,11 +54,7 @@ CREATE OR REPLACE VIEW goals_by_country_ranked as
     join players p 
     on p.player_id = ps.player_id 
     join countries c 
-<<<<<<< HEAD
-    on ps.nationality = c.country_code
-=======
     on p.nationality = c.country_code
->>>>>>> feature/database_normalisation
     group by country
     having sum(goals) > 0
     order by total_goals desc;
